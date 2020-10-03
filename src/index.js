2efeda624396417eb4950698cf9b59ef70e5fe8c
Olga Barysheva <olga.makarova.13@gmail.com>
2020-10-03 19:57:36 +0400

    feat: array methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(_array) {
+  if (!_array || !_array.length) return 0;
+
+  const array = [..._array];
+  array.sort((a, b) => a - b);
+  return array[0];
+
+
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max (_array) {
+  if (!_array || !_array.length) return 0;
+
+  const array = [..._array];
+  array.sort((a, b) => a - b);
+  return array[array.length-1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg1 = function avg(array) {
+  if (!array || !array.length) return 0;
+
+  let sum = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    sum = sum + array[i];
+  }
+
+  return sum / array.length;
+  
+}
+
+exports.avg = function avg(array) {
+  if (!array || !array.length) return 0;
+
+  const sum = array.reduce((acc, item) => acc + item); 
+  return sum / array.length;
 }
